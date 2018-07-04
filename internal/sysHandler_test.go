4e509f7d6cd7040e0bf4278e2881252e9a0b3a78
Alex Forbes <alex.forbes@starlingbank.com>
2018-07-04 15:19:23 +0100

    WIP; adding tests

--- internal/sysHandler_test.go
+++ internal/sysHandler_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/starlingbank/vaultsmith/mocks"
 	vaultApi "github.com/hashicorp/vault/api"
 	"testing"
+	"strings"
 )
 
 
@@ -13,13 +14,48 @@ func TestEnsureAuth(t *testing.T) {
 	client := &mocks.MockVaultsmithClient{}
 	sh, err := NewSysHandler(client, "")
 	if err != nil {
-		log.Fatalf("failed to create SysHandler (using mock client): %s", err)
+		log.Fatalf("Failed to create SysHandler: %s", err)
 	}
 
-	enableOpts := vaultApi.EnableAuthOptions{ }
+	enableOpts := vaultApi.EnableAuthOptions{}
 	err = sh.EnsureAuth("foo", enableOpts)
 	if err != nil {
 		log.Fatalf("Error calling EnsureAuth: %s", err)
 	}
 }
 
+func TestPutPoliciesFromEmptyDir(t *testing.T) {
+	client := &mocks.MockVaultsmithClient{}
+	sh, err := NewSysHandler(client, "")
+	if err != nil {
+		log.Fatalf("Failed to create SysHandler: %s", err)
+	}
+	err = sh.PutPoliciesFromDir("")
+	if err == nil {
+		log.Fatal("Expected error, got nil")
+	}
+	if ! strings.Contains(err.Error(), "FileInfo") {
+		log.Fatalf("Expected error about nil FileInfo, got '%s'", err.Error())
+	}
+
+}
+
+func TestPutPoliciesFromExampleDir(t *testing.T) {
+	client := &mocks.MockVaultsmithClient{}
+	sh, err := NewSysHandler(client, "./example")
+	if err != nil {
+		log.Fatalf("Failed to create SysHandler: %s", err)
+	}
+	err = sh.PutPoliciesFromDir("example/auth")
+	log.Println(err)
+
+}
+
+func TestSysHandlerWalkFile(t *testing.T) {
+	//client := &mocks.MockVaultsmithClient{}
+	//sh, err := NewSysHandler(client, "")
+	//if err != nil {
+	//	log.Fatalf("Failed to create SysHandler: %s", err)
+	//}
+
+}
