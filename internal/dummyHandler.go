012c845e3cc23c82b8d0b1372ed93184593d9fca
Alex Forbes <alex.forbes@starlingbank.com>
2018-07-04 15:16:37 +0100

    Add dummyHandler
    Useful for testing

---
+++ internal/dummyHandler.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"log"
+)
+
+// A minimal handler which does nothing, mainly for testing
+type DummyHandler struct {
+	BasePathHandler
+	client 				VaultsmithClient
+	rootPath 			string
+}
+
+func NewDummyHandler(c VaultsmithClient, rootPath string) (*DummyHandler, error) {
+	return &DummyHandler{
+		client: c,
+		rootPath: rootPath,
+	}, nil
+}
+
+func (sh *DummyHandler) PutPoliciesFromDir(path string) error {
+	log.Printf("Dummy handler got path: %s", path)
+	return nil
+}
---
+++ internal/dummyHandler_test.go
@@ -0,0 +1,15 @@
+package internal
+
+import (
+	"testing"
+	"github.com/starlingbank/vaultsmith/mocks"
+	"log"
+)
+
+func TestNewDummyHandler(t *testing.T) {
+	c := &mocks.MockVaultsmithClient{}
+	_, err := NewDummyHandler(c, "example")
+	if err != nil {
+		log.Fatalf("Failed to create dummy hanlder: %s", err.Error())
+	}
+}
