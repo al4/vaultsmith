154c78fc81934cd4328f4d4c54e836446b2a3b66
Alex Forbes <alex.forbes@starlingbank.com>
2018-07-10 16:04:22 +0100

    Package/Fetcher implementation
    Add filePath method and test
    
    Add ExtractPath()
    
    Add Package.CleanUp()
    
    Implement test for Package.Fetch()

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -6,29 +6,63 @@ import (
 	"net/http"
 	"io"
 	"net/url"
+	"log"
+	"strings"
 )
 
 type Package struct {
 	WorkDir	string
-	Url		url.URL
+	Url		*url.URL
 }
 
 type Fetcher interface {
-	Get() error
+	Fetch() error
 	Extract() error
+	ExtractPath() string
+	CleanUp()
 }
 
 
 // download tarball from Github
-func (p *Package) Get() {
-	err := p.downloader("vaultsmith.tar")
+func (p *Package) Fetch() (err error){
+	err = p.download()
 	if err != nil {
-		fmt.Errorf("error downloading tarball %s", err)
+		return fmt.Errorf("error downloading tarball %s", err)
 	}
+	return nil
 }
 
-func (p *Package) downloader(filepath string) error {
-	out, err := os.Create(filepath)
+func (p *Package) Extract() (path string, err error){
+
+	return
+}
+
+// Return the path to the extracted files
+// This does not ensure that the extract has actually been performed, so there is no guarantee the
+// directory exists
+func (p *Package) ExtractPath() (path string){
+	s := strings.Split(
+		strings.TrimRight(p.Url.Path, "/"),
+		"/")
+
+	dir := strings.TrimRight(p.WorkDir, string(os.PathSeparator))
+	subdir := "extract-" + s[len(s) - 1]
+
+	dirSlice := []string{dir, subdir}
+	return strings.Join(dirSlice, string(os.PathSeparator))
+}
+
+func (p *Package) CleanUp() {
+	log.Printf("Removing %s", p.WorkDir)
+	err := os.RemoveAll(p.WorkDir)
+	if err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func (p *Package) download() error {
+	out, err := os.Create(p.filePath())
 	if err != nil {
 		return err
 	}
@@ -40,15 +74,24 @@ func (p *Package) downloader(filepath string) error {
 	}
 	defer resp.Body.Close()
 
-	_, err = io.Copy(out, resp.Body)
+	n, err := io.Copy(out, resp.Body)
 	if err != nil {
 		return err
 	}
+	log.Printf("%v bytes written to %s", n, p.filePath())
 
 	return nil
 }
 
-func (p *Package) Extract() (path string, err error){
+func (p *Package) filePath() (path string) {
+	s := strings.Split(
+		strings.TrimRight(p.Url.Path, "/"),
+		"/")
 
-	return
+	dir := strings.TrimRight(p.WorkDir, string(os.PathSeparator))
+	file := s[len(s) - 1]
+
+	ns := []string{dir, file}
+	return strings.Join(ns, string(os.PathSeparator))
 }
+
--- fetcher/fetcher_test.go
+++ fetcher/fetcher_test.go
@@ -2,9 +2,80 @@ package fetcher
 
 import (
 	"testing"
+	"net/http/httptest"
+	"net/http"
+	"fmt"
+	"os"
+	"net/url"
+	"log"
+	"io/ioutil"
 )
 
-func TestDownloadFile(t *testing.T) {
-	f := Fetcher()
+type TestHttpHandler struct {
+	DummyData string
+}
+
+func (h *TestHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/gzip")
+	fmt.Fprintln(w, h.DummyData)
+}
+
+func TestPackage_filePath(t *testing.T) {
+	url, _ := url.Parse("https://example.com/test.tgz")
+	p := Package{
+		WorkDir: "/tmp/test",
+		Url: url,
+	}
+	exp := "/tmp/test/test.tgz"
+	r := p.filePath()
+	if r != exp {
+		log.Fatalf("Bad file path, expected %q, got %q", exp, r)
+	}
+}
+
+func TestPackage_ExtractPath(t *testing.T) {
+	url, _ := url.Parse("https://example.com/test.tgz")
+	p := Package{
+		WorkDir: "/tmp/test",
+		Url: url,
+	}
+	exp := "/tmp/test/extract-test.tgz"
+	r := p.ExtractPath()
+	if r != exp {
+		log.Fatalf("Bad extract path, expected %q, got %q", exp, r)
+	}
+}
+
+func TestPackage_Fetch(t *testing.T) {
+	expected := "dummy data"
+	ts := httptest.NewServer(&TestHttpHandler{
+		DummyData: expected,
+	})
+	url, _ := url.Parse(ts.URL + "/test-archive.tgz")
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "fetcher-")
+	if err != nil {
+		log.Fatalf("Could not create tempdir: %s", err)
+	}
+	p := Package{
+		WorkDir: tmpDir,
+		Url: url,
+	}
+
+	p.Fetch()
+	defer p.CleanUp()
+
+	if _, err := os.Stat(p.filePath()); os.IsNotExist(err) {
+		log.Fatalf("Expected file %s to exist", p.filePath())
+	}
+	c, err := ioutil.ReadFile(p.filePath())
+	if err != nil {
+		log.Fatalf("Error reading file %s", err)
+	}
+
+	// Something is adding a newline. It appears in the file, (so it must be present in the response
+	// Body), but shouldn't be a problem in real-world use
+	if string(c) != expected + "\n" {
+		log.Fatalf("Expected file contents to be %q, got %q", expected, c)
+	}
 }
 
